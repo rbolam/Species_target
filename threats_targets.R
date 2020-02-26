e9e54ac231cd20a5a35ce57f8bc38d529b055a98
rbolam <friederike.bolam@ncl.ac.uk>
2020-02-26 12:00:05 +0000

    Add annotation

--- threats_targets.R
+++ threats_targets.R
@@ -1,44 +1,49 @@
-## Making plot linking threats to targets
+# --------------------------- Making plot linking threats to targets ---------------------------####
 
-
+### Load packages
 library(tidyverse)
 library(ggalluvial)
 library(viridis)
 
-
+## --------------------------- Sort data --------------------------####
 threats <- read.csv("threats_targets.csv")
 
+### Separate threat codes for matching
 threats$code2 <- threats$code
 threats <- separate(threats, code2, into = c("threat_level1"), extra = "drop") 
 
+
+### Add threats 4 and 6 (Transportation & service corridors, Human intrusions & disturbance) to "other" 
+### as there were only 3% and 2% of observations in each one respectively
 threats$threat_level1[threats$threat_level1 %in% c(4, 6)] <- c("12")
 
-## Add in threat level 1 names
-
+### Add in threat level 1 names
 threat_level1 <- rep(1:12)
 tnames <- c("Residential & commercial\ndevelopment", "Agriculture & aquaculture", "Energy production & mining", 
             "Transportation & service corridors", "Biological resource use", "Human intrusions & disturbance",
             "Natural system modifications", "Invasive & other\nproblematic species",
             "Pollution", "Geological events", "Climate change &\nsevere weather", "Other")
 tnames <- data.frame(cbind(threat_level1, tnames))
-
 threats <- left_join(threats, tnames, by = "threat_level1")
 
+### Count how many threats in each target
 threats %>% 
   group_by(Aichi.Target, Zero.draft.Action.Target, tnames) %>% 
   summarise(n_new = sum(n)) ->
   threats_summ
 
-
+### Remove threat Geological events as they cannot be addressed by conservation action/policy
 threats_summ <- filter(threats_summ, tnames != "Geological events")
 
+### Reorder factors depending on size of threat, or numerical order of Target
 threats_summ %>% group_by(tnames) %>% summarise(sum = sum(n_new)) %>% arrange(sum)
 threats_summ$tnames <- factor(threats_summ$tnames, levels(threats_summ$tnames)[c(1, 2, 11, 7, 8, 3, 4, 10, 12, 6, 5, 9)])
 threats_summ$Aichi.Target <- factor(threats_summ$Aichi.Target, levels(threats_summ$Aichi.Target)[c(2, 3, 4, 5, 6, 1)])
 
 
 
-## Figure
+## --------------------------- Code for figure --------------------------####
+
 ggplot(threats_summ, aes(axis1 = Aichi.Target, axis2 = tnames, axis3 = Zero.draft.Action.Target, y = n_new)) +
   geom_alluvium(aes(fill = tnames), alpha = 0.9, aes.bind = TRUE, width = c(1/4, 1/2, 1/4)) +
   geom_stratum(size = 0.5, colour = "white", width = 
@@ -53,16 +58,12 @@ ggplot(threats_summ, aes(axis1 = Aichi.Target, axis2 = tnames, axis3 = Zero.draf
               c("black", "black", "black",
                 "black", "black", "black", "black", "white", "white", "white", "white", "white",
                 "black", "black", "black", "black")) +
-  #geom_label(stat = "stratum", infer.label = TRUE, size = 2, min.y = 1000, fill = "white",
-  #           label.padding = unit(0.08, "lines"), label.r = unit(0, "lines"), label.size = 0, alpha = 0.6) +
   scale_x_discrete(limits = c("Aichi Target", "Threat", "Post-2020 Target"), name = "", expand = c(.0005, 0)) + 
   scale_y_continuous(name = "Number of species affected per threat", expand = c(0.0005, 0)) + 
   scale_fill_viridis_d(option = "E", direction = 1) +
-  #scale_fill_brewer(palette = "YlGnBu", direction = -1) +
   theme_classic() +
   theme(legend.position = "none",
         text = element_text(size = 10),
-        #axis.text.x = element_blank(),
         axis.ticks.x = element_blank()) +
   annotate("text", x = 0.97, y = 8782, label = "Aichi Target 6", size = 2) +
   annotate("segment", x = 0.89, xend = 0.89, y = 8282, yend = 4921, size = 0.3) +
