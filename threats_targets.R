d47bd18b4a20d3e13dd30db61cb1f588c181065d
rbolam <friederike.bolam@ncl.ac.uk>
2020-02-26 11:33:37 +0000

    Change widths, update annotations

--- threats_targets.R
+++ threats_targets.R
@@ -4,7 +4,7 @@
 library(tidyverse)
 library(ggalluvial)
 library(viridis)
-library(cowplot)
+
 
 threats <- read.csv("threats_targets.csv")
 
@@ -40,39 +40,47 @@ threats_summ$Aichi.Target <- factor(threats_summ$Aichi.Target, levels(threats_su
 
 ## Figure
 ggplot(threats_summ, aes(axis1 = Aichi.Target, axis2 = tnames, axis3 = Zero.draft.Action.Target, y = n_new)) +
-  geom_alluvium(aes(fill = tnames), width = 1/2, alpha = 0.9, aes.bind = TRUE) +
-  geom_stratum(width = 1/2, size = 0.5, colour = "white", fill = 
+  geom_alluvium(aes(fill = tnames), alpha = 0.9, aes.bind = TRUE, width = c(1/4, 1/2, 1/4)) +
+  geom_stratum(size = 0.5, colour = "white", width = 
+                 c(1/4, 1/4, 1/4, 1/4, 1/4, 1/4, 
+                   1/2, 1/2, 1/2, 1/2, 1/2, 1/2, 1/2, 1/2, 1/2, 
+                   1/4, 1/4, 1/4, 1/4, 1/4), 
+               fill = 
                  c("grey90", "grey90", "grey90", "grey90", "grey90", "grey90", 
                    NA, NA, NA, NA, NA, NA, NA, NA, NA, 
                    "grey90", "grey90", "grey90", "grey90", "grey90")) +
-  #geom_text(stat = "stratum", infer.label = TRUE, size = 2, min.y = 1000, colour = "black") +
-  geom_label(stat = "stratum", infer.label = TRUE, size = 2, min.y = 1000, fill = "grey90",
-             label.padding = unit(0.08, "lines"), label.r = unit(0, "lines"), label.size = 0, alpha = 0.7) +
-  scale_x_discrete(limits = c("Aichi Target", "Threat", "Post-2020 Target"), name = "", expand = c(.05, 0)) + 
+  geom_text(stat = "stratum", infer.label = TRUE, size = 2, min.y = 1000, colour = 
+              c("black", "black", "black",
+                "black", "black", "black", "black", "black", "white", "white", "white", "white",
+                "black", "black", "black", "black")) +
+  #geom_label(stat = "stratum", infer.label = TRUE, size = 2, min.y = 1000, fill = "white",
+  #           label.padding = unit(0.08, "lines"), label.r = unit(0, "lines"), label.size = 0, alpha = 0.6) +
+  scale_x_discrete(limits = c("Aichi Target", "Threat", "Post-2020 Target"), name = "", expand = c(.0005, 0)) + 
   scale_y_continuous(name = "Number of species affected per threat", expand = c(0.0005, 0)) + 
   scale_fill_viridis_d(option = "E", direction = 1) +
+  #scale_fill_brewer(palette = "YlGnBu", direction = -1) +
   theme_classic() +
   theme(legend.position = "none",
         text = element_text(size = 10),
         #axis.text.x = element_blank(),
         axis.ticks.x = element_blank()) +
-  annotate("text", x = 0.6, y = 5782, label = "Aichi Target 6", size = 2) +
-  annotate("segment", x = 0.72, xend = 0.75, y = 5782, yend = 4921, size = 0.3) +
+  annotate("text", x = 0.97, y = 8782, label = "Aichi Target 6", size = 2) +
+  annotate("segment", x = 0.89, xend = 0.89, y = 8282, yend = 4921, size = 0.3) +
   
-  annotate("text", x = 0.6, y = 4482, label = "Aichi Target 7", size = 2) +
-  annotate("segment", x = 0.72, xend = 0.75, y = 4482, yend = 4482, size = 0.3) +
+  annotate("text", x = 1, y = 7482, label = "Aichi Target 7", size = 2) +
+  annotate("segment", x = 0.92, xend = 0.92, y = 6982, yend = 4482, size = 0.3) +
   
-  annotate("text", x = 0.6, y = 3182, label = "Aichi Target 8", size = 2) +
-  annotate("segment", x = 0.72, xend = 0.75, y = 3182, yend = 4016, size = 0.3) +
+  annotate("text", x = 1.03, y = 6182, label = "Aichi Target 8", size = 2) +
+  annotate("segment", x = 0.95, xend = 0.95, y = 5682, yend = 4016, size = 0.3) +
   
-  annotate("text", x = 3.4, y = 4471, label = "New Target 4", size = 2) +
-  annotate("segment", x = 3.25, xend = 3.28, y = 4471, yend = 4471, size = 0.3) +
+  annotate("text", x = 3.03, y = 8471, label = "New Target 4", size = 2) +
+  annotate("segment", x = 3.11, xend = 3.11, y = 4471, yend = 7971, size = 0.3) +
   
-  annotate("rect", xmin = 0.75, xmax = 1.25, ymin = 40279, ymax = 46665, fill = "grey90", colour = "white") +
-  annotate("text", x = 1, y = 43472, label = "Not covered\nby targets", size = 2) +
+  annotate("rect", xmin = 0.875, xmax = 1.125, ymin = 40279, ymax = 46665, fill = "grey30", colour = "white") +
+  annotate("text", x = 1, y = 43472, label = "Not addressed\nby targets", size = 2, colour = "white") +
   
-  annotate("rect", xmin = 2.75, xmax = 3.25, ymin = 42367, ymax = 46665, fill = "grey90", colour = "white") +
-  annotate("text", x = 3, y = 44516, label = "Not covered\nby targets", size = 2)
+  annotate("rect", xmin = 2.875, xmax = 3.125, ymin = 42367, ymax = 46665, fill = "grey30", colour = "white") +
+  annotate("text", x = 3, y = 44516, label = "Not addressed\nby targets", size = 2, colour = "white")
 
 ggsave("flow.png", width = 7, height = 5, dpi = 600)
 
