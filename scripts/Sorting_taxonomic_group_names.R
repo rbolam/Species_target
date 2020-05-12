b507f0e764869781b9cfd26ce269e56c2af3bb74
rbolam <friederike.bolam@ncl.ac.uk>
2020-05-12 13:32:55 +0100

    Add script to handle tax groups data

---
+++ scripts/Sorting_taxonomic_group_names.R
@@ -0,0 +1,20 @@
+## -------------- Sort out comprehensively assessed groups data -------------------####
+
+library(tidyverse)
+comp <- read.csv("data/Comprehensive_Taxonomic_Groups.csv", na.strings = c("", NA))
+
+
+## Remove " from some strings:
+comp$Taxonomy <- str_remove_all(comp$Taxonomy, "\"")
+comp <- separate(comp, Taxonomy, sep = "[=:]", into = c("tax1", "group1"))
+comp <- separate_rows(comp, group1, sep = "[Oo][r] ")
+
+comp <- separate(comp, Taxonomy2, sep = "[=:]", into = c("tax2", "group2"))
+comp <- separate_rows(comp, group2, sep = "[Oo][r] ")
+
+comp$group1_s <- str_squish(comp$group1)
+comp$group1_s == comp$group1
+
+for (i in 2:5) {
+  comp[,i] <-str_squish(comp[,i])
+}
